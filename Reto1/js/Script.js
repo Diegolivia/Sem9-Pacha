68d8415c46b31d770dae9c621389c550f1300886
Diego Livia <diegolivia@hotmail.com>
2020-05-30 09:49:38 -0500

    Añadir decript

--- Reto1/js/Script.js
+++ Reto1/js/Script.js
@@ -3,6 +3,9 @@ function Encript(CiphText, CiphNum) {
   [...CiphText].forEach((char) => {
     let car = char.charCodeAt() + CiphNum;
     switch (car) {
+        case 35:
+        car = 32;
+        break;
       case 93:
         car = 67;
         break;
@@ -34,3 +37,38 @@ function Encript(CiphText, CiphNum) {
   });
   console.log(str);
 }
+
+function Decript(CiphText, CiphNum) {
+    let str = "";
+    [...CiphText].forEach((char) => {
+      let car = char.charCodeAt() - CiphNum;
+      switch (car) {
+        case 29:
+            car = 32;
+            break;
+        case 62:
+          car = 88;
+          break;
+        case 63:
+          car = 89;
+          break;
+        case 64:
+          car = 90;
+          break;
+        case 94:
+          car = 120;
+          break;
+        case 95:
+          car = 121;
+          break;
+        case 96:
+          car = 122;
+          break;
+        default:
+          break;
+      }
+      str += String.fromCharCode(car);
+    });
+    console.log(str);
+  }
+  
